c9463259e95c99b65654df3aa733c8476ef350ce
David Frank <frankd@in.tum.de>
2020-03-31 01:12:25 +0200

    Remove .idea and build folder

--- build/CMakeFiles/tomosect.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libtomosect.a"
-  "libtomosect.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/tomosect.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
